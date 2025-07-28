f67b8f086dff7050f7e6ea3fb467eec17d6642cd
Marcos Silva <marcos.silva@persefoni.com>
2025-07-28 18:45:03 -0300

    feat: add cloud-init config and configure fw

--- infra/main.tf
+++ infra/main.tf
@@ -7,45 +7,105 @@ resource "hcloud_ssh_key" "main" {
   }
 }
 
+# Ensure a server is attached to a firewall on first boot
+# https://registry.terraform.io/providers/hetznercloud/hcloud/1.52.0/docs/resources/firewall_attachment#ensure-a-server-is-attached-to-a-firewall-on-first-boot
+# resource "hcloud_firewall" "deny_all_1" {
+#   name = "deny_all_1"
+# }
+
 # Create a new server running debian
 resource "hcloud_server" "node1" {
-  depends_on  = [hcloud_ssh_key.main]
-  name        = "vps1"
-  image       = "ubuntu-24.04"
+  name  = "vps1"
+  image = "ubuntu-24.04"
+  # ignore_remote_firewall_ids = true
   location    = "nbg1"
   server_type = "cx22"
   public_net {
     ipv4_enabled = true
     ipv6_enabled = false
   }
-  ssh_keys = [hcloud_ssh_key.main.name]
+  ssh_keys = [
+    hcloud_ssh_key.main.name
+  ]
   labels = {
     "env" : "dev"
   }
-  # user_data = ""
-  # firewall_ids = [hcloud_firewall.myfirewall.id]
+  user_data = templatefile("${path.module}/cloud-init.yaml.tftpl", {
+    ssh_publickey = var.ssh_publickey
+  })
+  # firewall_ids = [
+  #   hcloud_firewall.deny_all_1.id
+  # ]
 }
 
 # Configure Firewall
-# resource "hcloud_firewall" "myfirewall" {
-#   name = "my-firewall"
+
+# resource "hcloud_firewall" "allow_rules_1" {
+#   name = "allow_rules_1"
 #   rule {
-#     direction = "in"
-#     protocol  = "icmp"
+#     description = "PING"
+#     direction   = "in"
+#     protocol    = "icmp"
 #     source_ips = [
-#       "0.0.0.0/0",
-#       "::/0"
+#       "0.0.0.0/0"
 #     ]
 #   }
 
 #   rule {
-#     direction = "in"
-#     protocol  = "tcp"
-#     port      = "80-85"
+#     description = "HTTP"
+#     direction   = "in"
+#     protocol    = "tcp"
+#     port        = "80"
 #     source_ips = [
-#       "0.0.0.0/0",
-#       "::/0"
+#       "0.0.0.0/0"
 #     ]
 #   }
 
+#   rule {
+#     description = "HTTPS"
+#     direction   = "in"
+#     protocol    = "tcp"
+#     port        = "443"
+#     source_ips = [
+#       "0.0.0.0/0"
+#     ]
+#   }
+
+#   rule {
+#     description = "SSH"
+#     direction   = "in"
+#     protocol    = "tcp"
+#     port        = "2222"
+#     source_ips = [
+#       "0.0.0.0/0"
+#     ]
+#   }
+
+#   rule {
+#     description = "SSH"
+#     direction   = "in"
+#     protocol    = "tcp"
+#     port        = "22"
+#     source_ips = [
+#       "0.0.0.0/0"
+#     ]
+#   }
+
+#   labels = {
+#     "env" : "dev"
+#   }
+# }
+
+# resource "hcloud_firewall_attachment" "deny_all_att" {
+#   firewall_id = hcloud_firewall.deny_all_1.id
+#   server_ids = [
+#     hcloud_server.node1.id
+#   ]
+# }
+
+# resource "hcloud_firewall_attachment" "allow_rules_att" {
+#   firewall_id = hcloud_firewall.allow_rules_1.id
+#   server_ids = [
+#     hcloud_server.node1.id
+#   ]
 # }
